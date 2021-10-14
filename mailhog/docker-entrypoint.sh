a7940bc5d0f4d46817e971782089ce16c73918d8
Bruno Sinou <bruno.sinou@posteo.de>
2021-10-14 09:42:09 +0200

    Finalise Mailhog custom test image

--- mailhog/docker-entrypoint.sh
+++ mailhog/docker-entrypoint.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 set -e
 
-## Launch mailhog
-sudo -u mailhog /opt/mailhog/bin/mailhog
-
 ## Launch stunnel
 stunnel
 
-# Debug: list content of certs folder
-#echo "CAROOT is $CAROOT"
-#ls -lsah $CAROOT
-
-# TODO remove
-# keeps the container alive for debug purpose
-while (true); do 
- sleep 5
- echo "still running"
-done
+## Launch mailhog
+/bin/mailhog
